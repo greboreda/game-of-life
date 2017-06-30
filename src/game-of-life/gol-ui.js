f3cceb8166d7146aaeedaf5c2b76eb2379466fc6
Guillermo Reboreda <greboreda@idealista.com>
2017-06-30 14:55:51 +0200

    grid

--- src/game-of-life/gol-ui.js
+++ src/game-of-life/gol-ui.js
@@ -1,11 +1,66 @@
 $(document).ready(function() {
-	console.log('ready!');
+
+	var golConf = {
+		containerId: 'game-of-life',
+		width: 400,
+		height: 200,
+		rows: 10,
+		cols: 20
+	}
+
+	gameOfLifeManager.init(golConf);
 });
 
 var gameOfLifeManager = (function() {
+
 	var game = new gol.GameOfLife({
                         rows: 1,
                         cols: 1,
                         locationsWithLivingCell: []
                 });
+
+	function init(conf) {
+		initGrid(conf.containerId, conf.width, conf.height, conf.rows, conf.cols);
+	}
+
+	function initGrid(containerId, width, height, rows, cols) {
+		var gridConf = {
+			containerId: containerId,
+			width: width,
+			height: height,
+			rows: rows,
+			cols: cols
+		};
+
+		var grid = new Grid(gridConf);
+
+	}
+
+	return {
+		init: init
+	};
+
 })();
+
+
+class Grid {
+
+	constructor(conf) {
+		
+		this.containerId = conf.containerId;
+		this.width = conf.width;
+		this.height = conf.height;
+		this.rows = conf.rows;
+		this.cols = conf.cols;
+
+		this.canvas = $('<canvas/>').attr({width: this.width, height: this.height}).appendTo('#' + this.containerId);
+
+		var canv = this.canvas.get(0);
+
+		var ctx = canv.getContext("2d");
+		ctx.fillStyle = "black";
+		ctx.fillRect(0, 0, this.width, this.height);
+
+	}
+
+}
